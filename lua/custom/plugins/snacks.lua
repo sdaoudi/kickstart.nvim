2ad080541e94e13d698660291693d9de9ef6b8ab
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2025-01-27 23:27:10 +0100

    enable snacks

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    bufdelete = { enabled = true },
+    dashboard = { enabled = true },
+    debug = { enabled = false },
+    git = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = false },
+    picker = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+  },
+}
