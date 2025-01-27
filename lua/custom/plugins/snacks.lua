3437a1a1872cb58d9016424fdd9a523022d4a294
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2025-01-27 23:37:10 +0100

    Add auto session + update snack keys

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -17,4 +17,42 @@ return {
     scroll = { enabled = false },
     statuscolumn = { enabled = false },
   },
+  keys = {
+    {
+      '<leader>gB',
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = 'Git Browse',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>gb',
+      function()
+        Snacks.git.blame_line()
+      end,
+      desc = 'Git Blame Line',
+    },
+    {
+      '<leader>gf',
+      function()
+        Snacks.lazygit.log_file()
+      end,
+      desc = 'Lazygit Current File History',
+    },
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
+    {
+      '<leader>gl',
+      function()
+        Snacks.lazygit.log()
+      end,
+      desc = 'Lazygit Log (cwd)',
+    },
+  },
 }
