cbf78c4e77c57a60233f6696a7b1ba67fad830ad
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2025-01-28 00:44:36 +0100

    update lsp suports

--- init.lua
+++ init.lua
@@ -626,7 +626,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -634,7 +634,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -668,7 +668,22 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'bashls',
+        'css_variables',
+        'cssls',
+        'docker_compose_language_service',
+        'dockerls',
+        'html',
+        'intelephense',
+        'jsonls',
+        'lua_ls',
+        'markdown_oxide',
+        'pylyzer',
+        'sqlls',
         'stylua', -- Used to format Lua code
+        'tailwindcss',
+        'ts_ls',
+        'yamlls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
